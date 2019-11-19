15c238b2d1b3507aef6d4009e7582d55fd62d95e
Zsolt <zsolt@vandenmar.com>
2019-11-18 23:19:39 -0800

    ZSM - Update naming

--- Configuration.h
+++ Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(V1 Engineering, Ryan, 414)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(vandenmar)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Zsolt's CNC"
+#define CUSTOM_MACHINE_NAME "CNC"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
